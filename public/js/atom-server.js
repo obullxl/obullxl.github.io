2e338b43793f27c46347d6b6f35ec38731833adb
老牛啊 <obullxl@gmail.com>
2013-09-04 21:04:33 +0800

    add public resource Signed-off-by: 老牛啊 <obullxl@gmail.com>

---
+++ public/js/atom-server.js
@@ -0,0 +1,25 @@
+function fadeProgressModal(visiable) {
+	if (visiable) {
+		$("#modal-progress").modal("show");
+	} else {
+		$("#modal-progress").modal("hide");
+	}
+}
+
+function fadeSuccessModal(data) {
+	$("#success-biz-log").text(data.bizLog);
+	$("#modal-success").modal("show");
+}
+
+function fadeFailureModal(data) {
+	$("#failure-biz-log").text(data.bizLog);
+	$("#failure-resp-code").text(data.respCode);
+	$("#failure-resp-desp").text(data.respDesp);
+	$("#modal-failure").modal("show");
+}
+
+/*
+setTimeout(function() {
+	$('.sidebar-nav').affix()
+}, 100);
+*/
