08650a616d122605491efeb5fea1f28a525e9b60
老牛啊 <obullxl@163.com>
2023-04-29 21:26:25 +0800

    Baidu submit

---
+++ static/urls/submit.sh
@@ -0,0 +1,5 @@
+curl -H 'Content-Type:text/plain' --data-binary @20230429.txt "http://data.zz.baidu.com/urls?site=https://ntopic.cn&token=udC5bg92zRQGFDbJ"
+
+# https://ziyuan.baidu.com/linksubmit/index
+
+
