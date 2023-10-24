25773ddc7a6f133c68a994bbe86de9f7ba80ee30
老牛啊 <obullxl@163.com>
2023-10-24 22:21:32 +0800

    Add Gitee.IO config

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 sudo rm -rf ./public
 sudo ln -s ../obullxl-gitee/public ./public
-sudo hugo -D
+sudo hugo -D --config config-GiteeIO.yaml
 cd ../obullxl-gitee
 git add --all
 git commit -m 'Deploy public'
